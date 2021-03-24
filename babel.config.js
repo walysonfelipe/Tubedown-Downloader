03e15d4d5b0189a13459343df0b022af12e9e8b4
Walyson Felipe <walysonfelipe25@gmail.com>
2021-03-24 20:55:32 -0300

    :heavy_plus_sign: Adicionando dependencias

---
+++ babel.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ],
+    '@babel/preset-typescript'
+  ],
+  plugins: [
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['module-resolver', {
+      alias: {
+        '@config': './src/config',
+        '@models': './src/models',
+        '@controllers': './src/controllers',
+        '@views': './src/views',
+        '@utils': './src/utils'
+      }
+    }]
+  ],
+  ignore: [
+    '**/*.spec.ts'
+  ]
+}
